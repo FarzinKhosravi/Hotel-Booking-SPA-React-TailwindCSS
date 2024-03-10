ef34a0106268822afa44f6344118c48e7421c4a4
FarzinKhosravi <khosravii.farzin@gmail.com>
2024-03-10 12:45:31 +0330

    updated NotFoundPage component

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -8,7 +8,7 @@ function NotFoundPage() {
         <div className="mx-auto max-w-lg text-gray-600">
           <div className="mb-8 text-center">
             <div className="mb-4 flex items-center justify-center text-xl font-semibold text-red-600 lg:text-3xl">
-              <span className="text-5.5 lg:text-8 -mt-0.25 inline-block">
+              <span className="-mt-0.25 inline-block text-5.5 lg:text-8">
                 404
               </span>
               <span className="ml-1 inline-block">ERROR</span>
@@ -103,6 +103,50 @@ function NavigationList() {
           ? "/bookmarks?mapTitle=Bookmarks List"
           : "/login?redirect=bookmarksList"
       } `,
+    },
+    {
+      id: 4,
+      icon: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth="1.5"
+          stroke="currentColor"
+          className="h-6 w-6 text-slate-100 lg:h-7 lg:w-7"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M14.25 9.75v-4.5m0 4.5h4.5m-4.5 0 6-6m-3 18c-8.284 0-15-6.716-15-15V4.5A2.25 2.25 0 0 1 4.5 2.25h1.372c.516 0 .966.351 1.091.852l1.106 4.423c.11.44-.054.902-.417 1.173l-1.293.97a1.062 1.062 0 0 0-.38 1.21 12.035 12.035 0 0 0 7.143 7.143c.441.162.928-.004 1.21-.38l.97-1.293a1.125 1.125 0 0 1 1.173-.417l4.423 1.106c.5.125.852.575.852 1.091V19.5a2.25 2.25 0 0 1-2.25 2.25h-2.25Z"
+          />
+        </svg>
+      ),
+      title: "Contact Us Page",
+      desc: "Get in Touch with Us ☎️",
+      href: "/contact-us",
+    },
+    {
+      id: 5,
+      icon: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth="1.5"
+          stroke="currentColor"
+          className="h-6 w-6 text-slate-100 lg:h-7 lg:w-7"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M18 18.72a9.094 9.094 0 0 0 3.741-.479 3 3 0 0 0-4.682-2.72m.94 3.198.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0 1 12 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 0 1 6 18.719m12 0a5.971 5.971 0 0 0-.941-3.197m0 0A5.995 5.995 0 0 0 12 12.75a5.995 5.995 0 0 0-5.058 2.772m0 0a3 3 0 0 0-4.681 2.72 8.986 8.986 0 0 0 3.74.477m.94-3.197a5.971 5.971 0 0 0-.94 3.197M15 6.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm6 3a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Zm-13.5 0a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Z"
+          />
+        </svg>
+      ),
+      title: "About Us Page",
+      desc: "If you Want to Get to Know Our Group, Please Click on the Link Below 📑",
+      href: "/about-us",
     },
   ];
 
