0fce7c3d7634a7de31132b1e3d72070dc450c4f0
FarzinKhosravi <khosravii.farzin@gmail.com>
2024-03-28 20:03:26 +0330

    JSON Server went online

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:3000";
+axios.defaults.baseURL = "https://hotel-booking-db.onrender.com/";
 
 const http = {
   get: axios.get,
